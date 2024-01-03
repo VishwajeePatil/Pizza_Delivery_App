7e4a5131f0290b75291a8697e7126c4e938ccc8b
VishwajeePatil <techtour1620@gmail.com>
2024-01-03 14:48:32 +0530

    signup backend Created

---
+++ Backend/Model/UserModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  mobileNo: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  getOffer: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const UserModel = mongoose.model('Users', userSchema);
+
+module.exports = {UserModel};
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Pizza_Delivery_App
+
+
+<h2>API Endpoints</h2>
+/signup -> To Signup (requires name, mobile no, email, dob, password, getOffer)
