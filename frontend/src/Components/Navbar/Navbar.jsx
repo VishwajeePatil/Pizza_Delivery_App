7f772b6ded779eadae81b7ef558a17ce8afc9fdc
VishwajeePatil <techtour1620@gmail.com>
2024-01-04 09:35:51 +0530

    Navbar Responsiveness added GSAP

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from "./Navbar.module.css"
+
+import { gsap } from "gsap";
 import {Link} from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faPizzaSlice, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const [active_menu,set_active_menu] = useState(false)
+  const toggel_menu_btn = ()=>{
+    gsap.to(`.${style.links}`,{
+      duration: 0.2,
+      height: active_menu ? '0' : '230',
+      ease: 'power2.inOut'
+    })
+    set_active_menu(!active_menu);
+  }
   return (
     <div className={style.main}>
         <div className="container flex">
@@ -16,6 +27,9 @@ const Navbar = () => {
                 <Link className={style.link} to="/OrderTracking">Track Order</Link>
                 <Link className={style.link} to="/cart">Cart</Link>
                 <Link className={style.link} to="/auth/signup">Signup | Login</Link>
+            </div>
+            <div className={style.menubtn}>
+              <FontAwesomeIcon icon={faHamburger} onClick={toggel_menu_btn}/>
             </div>
         </div>
     </div>
