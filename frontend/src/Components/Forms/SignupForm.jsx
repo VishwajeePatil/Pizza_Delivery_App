99579fab1224ac45dbbade0e0808c7884b4082df
VishwajeePatil <techtour1620@gmail.com>
2024-01-04 23:04:01 +0530

    Signup Form Created

--- frontend/src/Components/Forms/SignupForm.jsx
+++ frontend/src/Components/Forms/SignupForm.jsx
@@ -1,17 +1,58 @@
 import React, { useState } from 'react'
-import style from "./SignupForm.module.css"
+import styles from "./SignupForm.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faEye  , faEyeSlash} from '@fortawesome/free-solid-svg-icons';
+
 const SignupForm = () => {
-    const [formData,setformData] = useState({
-        name:"",
-        mobileNo:"",
-        email:"",
-        dob:"",
-        password:"",
-        getOffer:false
-    })
-  return (
-    <div className={style.main}>
-      
+  const [typePass,settypePass] = useState(true)
+  const changepasstype = ()=>{
+    settypePass(!typePass)
+  }
+  const [formData, setFormData] = useState({
+    name: "",
+    mobileNo: "",
+    email: "",
+    dob: "",
+    password: "",
+    getOffer: false
+  })
+  return(
+    <div className={styles.formcontainer}>
+      <div className={styles.name}>
+        <label>Full Name</label>
+        <input type="text" placeholder='Enter Full Name' />
+      </div>
+
+      <div className={styles.email}>
+        <label>Email</label>
+        <div>
+        <input type="email" placeholder='Enter Email Address' />
+        <button>Send OTP</button>
+        </div>
+      </div>
+
+      <div className={styles.email}>
+        <label>Enter OTP</label>
+        <div>
+        <input type="text" placeholder='_ _ _   _ _ _'/>
+        <button>Verify OTP</button>
+        </div>
+      </div>
+
+      <div className={styles.password}>
+        <label> Password </label>
+        <input type={typePass ? "password" : "text"} placeholder='* * * * * * * * ' />
+        <FontAwesomeIcon icon={typePass ? faEyeSlash : faEye } className={styles.eyeIcon} onClick={changepasstype}/>
+      </div>
+      <div className={styles.getoffer}>
+        <label>Get Updates Of Exiting Offer</label>
+        <input type="checkbox" />
+        <div className={styles.circleMark}></div>
+      </div>
+
+      <div>
+        <input type="submit" />
+      </div>
     </div>
   )
 }
